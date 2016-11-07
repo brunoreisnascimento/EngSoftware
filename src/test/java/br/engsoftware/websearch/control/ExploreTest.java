fb903dcd2e3ac1a98cddeded030ac8bfea09d5ae
cleversonc6 <cleversonc6@gmail.com>
2016-11-07 17:43:45 -0300

    simples correção de nome

--- src/test/java/br/engsoftware/websearch/control/ExploreTest.java
+++ src/test/java/br/engsoftware/websearch/control/ExploreTest.java
@@ -3,8 +3,6 @@ package br.engsoftware.websearch.control;
 import br.engsoftware.websearch.model.News;
 import br.engsoftware.websearch.model.Site;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
